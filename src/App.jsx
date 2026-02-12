8338a0fd08663428f067b7a7d7c5cc84ff25a84a
PRANTO KUMER PAUL <prantopaul55555@gmail.com>
2026-02-12 14:40:09 +0600

    jsx & styles implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ const App = () => {
   return (
     <div>
       <h1>Counter</h1>
+      <div>
+        <button>+</button>
+        <button style={{margin: "0 20px"}}>Reset</button>
+        <button>-</button>
+      </div>
+      <div style={{margin: "10px 0"}}>
+        <input type="text"
+        className="input-box"
+        />
+        <button style={{margin:"0 8px"}}>set</button>
+      </div>
     </div>
   )
 }
