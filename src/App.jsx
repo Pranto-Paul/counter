04343b88563970c4a502bbf97c5f8409df8d36fc
PRANTO KUMER PAUL <prantopaul55555@gmail.com>
2026-02-12 15:12:20 +0600

    - add useState - use the state variable - update the state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import "./App.css"
+import { useState } from 'react';
+import './App.css';
 const App = () => {
+  const [count, setCount] = useState(7);
   return (
     <div>
-      <h1>Counter</h1>
+      <h1>Counter: {count}</h1>
       <div>
-        <button>+</button>
-        <button style={{margin: "0 20px"}}>Reset</button>
-        <button>-</button>
+        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+
+        <button onClick={() => setCount(0)} style={{ margin: '0 20px' }}>
+          Reset
+        </button>
+
+        <button onClick={() => setCount(count - 1)}>-</button>
       </div>
-      <div style={{margin: "10px 0"}}>
-        <input type="text"
-        className="input-box"
-        />
-        <button style={{margin:"0 8px"}}>set</button>
+      <div style={{ margin: '10px 0' }}>
+        <input type="text" className="input-box" />
+
+        <button style={{ margin: '0 8px' }}>set</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
