5d82e1c47694c2e82c6857bb66a2728ed01c3887
PRANTO KUMER PAUL <prantopaul55555@gmail.com>
2026-02-12 15:20:04 +0600

    - add another state variable using useState - handle inputfield with state variables - handle submission

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react';
 import './App.css';
 const App = () => {
   const [count, setCount] = useState(7);
+  const [inputVal, setInputVal] = useState(0);
+  const handleSubmit = () => {
+    setCount(Math.max(inputVal, 0));
+    setInputVal(0);
+  };
   return (
     <div>
       <h1>Counter: {count}</h1>
@@ -15,9 +20,16 @@ const App = () => {
         <button onClick={() => setCount(count - 1)}>-</button>
       </div>
       <div style={{ margin: '10px 0' }}>
-        <input type="text" className="input-box" />
+        <input
+          type="text"
+          className="input-box"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
 
-        <button style={{ margin: '0 8px' }}>set</button>
+        <button onClick={handleSubmit} style={{ margin: '0 8px' }}>
+          set {inputVal}
+        </button>
       </div>
     </div>
   );
